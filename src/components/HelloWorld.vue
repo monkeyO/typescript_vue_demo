dbdc97b878d61b7da489963745a05214914244dd
hlk <3204666986@qq.com>
2020-03-19 11:03:42 +0800

    提交所有

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ name }}</h1>
+    <h2></h2>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({ name: "Hello" })
+export default class Hello extends Vue {
+  private name: string = "张三";
+  private sex: string = "男";
+
+  setName(val: string = "测试数据") {
+    console.log(val);
+  }
+
+  mounted() {
+    this.setName();
+  }
+}
+</script>
+<style lang="less" scoped></style>
